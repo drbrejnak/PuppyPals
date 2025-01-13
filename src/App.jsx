ff62e9af53a70e6b88eebe4f759d71ff82259dd1
Dani <drbrejnak@gmail.com>
2025-01-13 18:44:04 -0500

    CSS update

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,24 @@ function App() {
   console.log(featuredPup)
 
   return (
-    <div>
-      {
-        puppies.map((puppy) => {
-          return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
-        })
-      }
-      {featPupId && (
-        <div>
-          <h2>{featuredPup.name}</h2>
-          <ul>
-            <li>Age: {featuredPup.age}</li>
-            <li>Email: {featuredPup.email}</li>
-          </ul>
-        </div>
-      )}
-    </div>
+    <>
+      <div className='roster'>
+        {
+          puppies.map((puppy) => {
+            return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          })
+        }
+      </div>
+        {featPupId && (
+          <div className='individual'>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+    </>
   )
 }
 
